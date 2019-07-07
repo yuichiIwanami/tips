8e4f188d471b699a1f7b2eeea75a3b73d2a07348
yuichiIwanami <gaudi8@gmail.com>
2019-07-08 00:55:58 +0900

    Create easy_dict.py

---
+++ easy_dict.py
@@ -0,0 +1,8 @@
+# Convenience class that behaves exactly like dict(), but allows accessing
+# the keys and values using the attribute syntax, i.e., "mydict.key = value".
+
+class EasyDict(dict):
+    def __init__(self, *args, **kwargs): super().__init__(*args, **kwargs)
+    def __getattr__(self, name): return self[name]
+    def __setattr__(self, name, value): self[name] = value
+    def __delattr__(self, name): del self[name]
