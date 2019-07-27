999c512eaf12566134cb26676135b601f88eb361
yuichiIwanami <gaudi8@gmail.com>
2019-07-27 10:58:14 +0900

    Update easy_dict.py

--- easy_dict.py
+++ easy_dict.py
@@ -2,7 +2,11 @@
 # the keys and values using the attribute syntax, i.e., "mydict.key = value".
 
 class EasyDict(dict):
-    def __init__(self, *args, **kwargs): super().__init__(*args, **kwargs)
-    def __getattr__(self, name): return self[name]
-    def __setattr__(self, name, value): self[name] = value
-    def __delattr__(self, name): del self[name]
+    def __init__(self, *args, **kwargs): 
+        super().__init__(*args, **kwargs)
+    def __getattr__(self, name): 
+        return self[name]
+    def __setattr__(self, name, value): 
+        self[name] = value
+    def __delattr__(self, name): 
+        del self[name]
